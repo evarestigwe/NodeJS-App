492f3e472e3b1e41bf821ce3a7be29bfd02dc108
evarest <evarest.igwe@harmonykloud.com>
2025-09-24 18:46:08 +0100

    update index.js

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From EVAREST IGWE TO LIGHT IHECHUKWU OKROWU,I LOVE YOU" }');
+    res.send('{ "response": "Hello From EVAREST IGWE TO LIGHT IHECHUKWU OKROWU , I LOVE YOU" }');
 });
 
 app.get('/will', function (req, res) {
     res.send('{ "response": "Hello World" }');
 });
 app.get('/ready', function (req, res) {
-    res.send('{ "response": " Great!, It works!" }');
+    res.send('{ "response": "WE ARE Great!, It works!" }');
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;
