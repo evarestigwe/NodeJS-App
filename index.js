3bf2a8f7497b72966c65fc2f800ee6459e885166
evarest <evarest.igwe@harmonykloud.com>
2025-09-24 18:37:41 +0100

    update pipeline

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Thetips4you" }');
+    res.send('{ "response": "Hello From EVAREST IGWE TO LIGHT IHECHUKWU OKROWU,I LOVE YOU" }');
 });
 
 app.get('/will', function (req, res) {
